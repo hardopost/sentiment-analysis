e7b811cda29bc14c6c81e60f85cec772f9060475
hardopost <hardopost@gmail.com>
2025-04-03 13:26:41 +0300

    Changed LLM model from OpenAI GPT4o to Google Gemini 2.0 flash, closes #9

--- backend/src/main/java/com/hardo/sentimentanalysis/Application.java
+++ backend/src/main/java/com/hardo/sentimentanalysis/Application.java
@@ -1,6 +1,8 @@
 package com.hardo.sentimentanalysis;
 
 import com.hardo.sentimentanalysis.processing.StatementProcessingService;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,7 +15,7 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
+	/*@Bean
 	CommandLineRunner run(StatementProcessingService statementProcessingService) {
 		return args -> {
 			try {
@@ -21,6 +23,19 @@ public class Application {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
+		};*/
+
+	/*@Bean
+	CommandLineRunner commandLineRunner(ChatClient.Builder builder) {
+		return args -> {
+			var chat = builder.build();
+
+			ChatResponse response = chat.prompt("Tell me an interesting fact about Google")
+					.call()
+					.chatResponse();
+
+			System.out.println(response);
+
 		};
-	}
+	}*/
 }
